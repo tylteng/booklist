3bb39c22fa5264b271643d9baaef13c046202c40
tylteng <teng.ty4@gmail.com>
2017-12-17 18:38:33 -0500

    ability to render plain text AND export as csv(excel)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,13 @@
 class BooksController < ApplicationController
 
   def index
+    books = Book.all
     respond_to do |format|
       format.html
+      format.text
+      format.csv do
+        render plain: Book.generate_csv(books)
+      end
     end
   end
 
